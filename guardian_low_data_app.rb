bb941e14aa4c0ffca72d2ad7de39a23324d22666
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2017-08-16 15:58:36 +0200

    Adding before Filter and setting api key inside

--- guardian_low_data_app.rb
+++ guardian_low_data_app.rb
@@ -1,16 +1,15 @@
-
 class GuardianLowDataApp < Sinatra::Base
 
-  GUARDIAN_CONTENT_API_KEY = ENV['GUARDIAN_CONTENT_API_KEY']
- 
   set :haml, :format => :html5
 
   configure :development do
     use BetterErrors::Middleware
     BetterErrors.application_root = File.expand_path('..', __FILE__)
   end
-
-  GuardianContent::Base.new
+  
+  before do
+    GuardianContent::Base.new(ENV['GUARDIAN_CONTENT_API_KEY'])
+  end
 
   get '/' do
     @results = GuardianContent::Content.search("Britain", order: 'newest', select: { fields: :all } )
