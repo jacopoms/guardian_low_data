252f6c21e29cc6c33a05c068da83a9b22f82a409
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2017-08-25 11:49:30 +0200

    added pagination style

--- app/custom_link_renderer.rb
+++ app/custom_link_renderer.rb
@@ -3,11 +3,6 @@ require 'will_paginate/view_helpers'
 
 class CustomLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
   def url(page)
-    str = request.path_info
-    str << "/page/#{page}"
-  end
-
-  def request
-    @template.request
+    "/page/#{page}"
   end
 end
