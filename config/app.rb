7b8cccf07d48698d4d2dcb8be0c4cd3e2fd3cea8
Jackopo <jacopo.ghisolfi@gmail.com>
2020-08-15 16:22:02 +0200

    serving css as static file

--- config/app.rb
+++ config/app.rb
@@ -4,9 +4,6 @@ require 'sinatra'
 require 'better_errors'
 require 'will_paginate/view_helpers/sinatra'
 require 'will_paginate/array'
-require 'sprockets'
-require 'sass'
-
 # pull in the helpers and controllers
 Dir.glob('./app/{helpers,controllers}/*.rb').each { |file| require file }
 # :nodoc:
@@ -17,19 +14,11 @@ class GuardianLowDataApp < ApplicationController
   set :haml, format: :html5
   set :logging, true
   set :server, %(thin)
-  set :views, File.expand_path('../../app/views', __FILE__)
-
+  set :views, File.expand_path('../app/views', __dir__)
+  set :public_folder, 'public'
   enable :sessions
+
   helpers Sinatra::GuardianLowDataApp::Helpers
-
-  configure do
-    set :sprockets_env, Sprockets::Environment.new # initialize new sprockets environment
-  end
-  # append assets paths
-  settings.sprockets_env.append_path 'app/assets/stylesheets'
-
-  # compress assets
-  settings.sprockets_env.css_compressor = :scss
 
   configure :development do
     set :bind, '0.0.0.0'
@@ -47,11 +36,5 @@ class GuardianLowDataApp < ApplicationController
 
   before do
     GuardianContent.new(ENV['GUARDIAN_CONTENT_API_KEY'])
-  end
-
-  # get assets
-  get '/assets/*' do
-    env['PATH_INFO'].sub!('/assets', '')
-    settings.sprockets_env.call(env)
   end
 end
