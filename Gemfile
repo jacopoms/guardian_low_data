e4da47c057b44b0b50d6b3ee9c6c24e96858b7ec
Jackopo <jacopo.ghisolfi@gmail.com>
2021-01-30 16:13:33 +0100

    updated Ruby version

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
-ruby '2.6.3'
+ruby '3.0.0'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
@@ -16,12 +16,10 @@ gem 'thin'
 gem 'will_paginate', '~> 3.1.1'
 
 group :development do
-  gem 'solargraph'
-  gem 'rubocop'
+  gem 'irb'
 end
 
 group :test do
-  gem 'minitest'
   gem 'rack-test'
   gem 'rake'
   gem 'rspec'
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,31 +3,38 @@ require_relative 'custom_link_renderer'
 module Sinatra
   module GuardianLowDataApp
     module Helpers
+
       def path_info
         '/' if request.path_info == '/search'
         request.path_info
       end
 
-      def prepare_articles(results)
-        if results.empty?
-          [OpenStruct.new(title: 'No Articles')]
-        else
-          results
-        end
-      end
-
       def render_articles(query = nil)
-        articles = GuardianContent::Content.search(
+        @articles ||= GuardianContent::Content.search(
           query,
           order: 'newest',
           limit: 200,
           select: { fields: :all }
         ).paginate(page: @page, per_page: 10)
-        haml :home, locals: { results: prepare_articles(articles) }
+        p "articles = #{@articles}/n"
+        haml :home, locals: { results: format_articles }
       end
 
       def set_path
         session[:request_path] = path_info
+      end
+
+      private
+
+      attr_reader :articles
+
+      def format_articles
+        p "@articles #{@articles}}/n"
+        if @articles.empty?
+          [OpenStruct.new(title: 'No Articles')]
+        else
+          @articles
+        end
       end
     end
   end
