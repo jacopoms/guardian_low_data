2ef67942539b5559d8f864c3932e831f4b357fbb
Jackopo <jacopo.ghisolfi@gmail.com>
2022-02-09 08:05:54 +0100

    changed controller method name
    Co-authored-by: Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,39 +4,39 @@ class ApplicationController < Sinatra::Base
   set :views, File.expand_path('views', __dir__)
 
   get '/' do
-    set_page
-    set_query
+    page
+    query
     set_path
     render_articles
   end
 
   get '/page/:page' do |page|
-    set_page(page)
-    set_query(session[:query])
+    page(page)
+    query(session[:query])
     set_path
     render_articles(@query)
   end
 
   get '/article/*' do |id|
     @back_path = session[:request_path]
-    set_query(session[:query])
+    query(session[:query])
     @article = GuardianContent::Content.find_by_id(id)
     haml :article
   end
 
   post '/search' do
-    set_query(params[:q])
+    query(params[:q])
     render_articles(@query)
   end
 
   private
 
-  def set_query(query = nil)
+  def query(query = nil)
     session[:query] = query
     @query = session[:query]
   end
 
-  def set_page(page = 1)
+  def page(page = 1)
     @page = page.to_i
   end
 end
